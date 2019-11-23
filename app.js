5449a175495ad8343eef24367d22d14a9eb063ba
Ellen <evwhite2@gmail.com>
2019-11-23 11:26:24 -0600

    get and first post route added

--- app.js
+++ app.js
@@ -5,12 +5,40 @@ const path = require("path");
 var app = express();
 var PORT = 8000;
 
-var reservations = [];
+var reservations = [
+  {
+    rsvpName: "Ellen",
+    email: "example@example.com",
+    phone:"815-123-4567",
+    id:"12345"
+  },
+  {
+    rsvpName: "Cater",
+    email: "example@example.com",
+    phone:"815-123-4567",
+    id:"12346"
+  }
+];
+
+var waitList = [
+  {
+    rsvpName: "Raquel",
+    email: "example@example.com",
+    phone:"815-123-4567",
+    id:"12347"
+  },
+  {
+    rsvpName: "Annissa",
+    email: "example@example.com",
+    phone:"815-123-4567",
+    id:"12348"
+  }
+]
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//html routes
+//HTML routes
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./home.html"));
 });
@@ -24,6 +52,23 @@ app.get("/reserve", function(req, res) {
 });
 
 
+//API Routes
+
+app.get("/api/reservations", function(req, res){
+  return res.json(reservations);
+});
+
+app.get("/api/waitList", function(req, res){
+  return res.json(waitList);
+});
+
+app.post("/api/tables", function(req, res){
+  var newRSVP = req.body;
+  waitList.push(newRSVP);
+  res.json(false);
+})
+
+//listen on port
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
